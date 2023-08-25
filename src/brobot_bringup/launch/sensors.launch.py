16b5f5cde48f7a0850ffcaf234d86abb858e9be8
Atticus Russell <atticusrussell@gmail.com>
2023-08-24 21:55:04 -0400

    removed unecessary sensors

--- src/brobot_bringup/launch/sensors.launch.py
+++ src/brobot_bringup/launch/sensors.launch.py
@@ -25,32 +25,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
     laser_sensor_name = os.getenv('BROBOT_LASER_SENSOR', '')
     base_laser_sensor_name = os.getenv('BROBOT_BASE_LASER_SENSOR', '')
-    depth_sensor_name = os.getenv('BROBOT_DEPTH_SENSOR', '')
     
     fake_laser_config_path = PathJoinSubstitution(
         [FindPackageShare('brobot_bringup'), 'config', 'fake_laser.yaml']
     )
 
-    #indices
-    #0 - depth topic (str)
-    #1 - depth info topic (str)
-    depth_sensors = {
-        '': ['', '', '', {}, '', ''],
-        'realsense': ['/camera/depth/image_rect_raw', '/camera/depth/camera_info'],
-        'astra': ['/depth/rgb/ir', '/camera_info'],
-        'zed': ['/zed/depth/depth_registered', '/zed/depth/camera_info'],
-        'zed2': ['/zed/depth/depth_registered', '/zed/depth/camera_info'],
-        'zed2i': ['/zed/depth/depth_registered', '/zed/depth/camera_info'],
-        'zedm': ['/zed/depth/depth_registered', '/zed/depth/camera_info']
-    }
+    
 
     laser_launch_path = PathJoinSubstitution(
         [FindPackageShare('brobot_bringup'), 'launch', 'lasers.launch.py']
     )
 
-    depth_launch_path = PathJoinSubstitution(
-        [FindPackageShare('brobot_bringup'), 'launch', 'depth.launch.py']
-    )
+    
 
     camera_launch_path = PathJoinSubstitution(
         [FindPackageShare('brobot_bringup'), 'launch', 'camera.launch.py']
@@ -74,22 +60,6 @@ def generate_launch_description():
         ),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(camera_launch_path)
-        ),
-            
-
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(depth_launch_path),
-            condition=IfCondition(PythonExpression(['"" != "', depth_sensor_name, '"'])),
-            launch_arguments={'sensor': depth_sensor_name}.items()   
-        ),
-
-        Node(
-            condition=IfCondition(PythonExpression(['"" != "', laser_sensor_name, '" and ', '"', laser_sensor_name, '" in "', str(list(depth_sensors.keys())[1:]), '"'])),
-            package='depthimage_to_laserscan',
-            executable='depthimage_to_laserscan_node',
-            remappings=[('depth', depth_sensors[depth_sensor_name][0]),
-                        ('depth_camera_info', depth_sensors[depth_sensor_name][1])],
-            parameters=[fake_laser_config_path]
-        )
+        ),   
     ])
 
