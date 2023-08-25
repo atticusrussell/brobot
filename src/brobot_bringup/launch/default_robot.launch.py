3741cd5e466f78ee4750a43a257a001ea99c2f3c
Atticus Russell <atticusrussell@gmail.com>
2023-08-24 21:01:40 -0400

    added linorobot2 renamed packages and colcon still builds

---
+++ src/brobot_bringup/launch/default_robot.launch.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2021 Juan Miguel Jimeno
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition
+
+
+def generate_launch_description():
+    sensors_launch_path = PathJoinSubstitution(
+        [FindPackageShare('brobot_bringup'), 'launch', 'sensors.launch.py']
+    )
+
+    description_launch_path = PathJoinSubstitution(
+        [FindPackageShare('brobot_description'), 'launch', 'description.launch.py']
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            name='base_serial_port', 
+            default_value='/dev/ttyACM0',
+            description='Linorobot Base Serial Port'
+        ),
+        Node(
+            package='micro_ros_agent',
+            executable='micro_ros_agent',
+            name='micro_ros_agent',
+            output='screen',
+            arguments=['serial', '--dev', LaunchConfiguration("base_serial_port")]
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(description_launch_path)
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(sensors_launch_path),
+        )
+    ])
