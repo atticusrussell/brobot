38b7497d1b3f8e4f27ddad399e05ec75eabbe34b
Atticus Russell <atticusrussell@gmail.com>
2023-08-25 22:31:50 -0400

    fix erroring test

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-vcs import < src/ros2.repos src
+# the following commented out because empty breaks stuff
+# vcs import < src/ros2.repos src
 sudo apt-get update
 rosdep update
-rosdep install --from-paths src --ignore-src -y
+rosdep install --from-paths src --ignore-src -y --skip-keys micro_ros_agent
